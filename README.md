a4d83ead7d9bf99eab2e243659fb370d49eade91
matbahasa <nomoto@tufs.ac.jp>
2023-01-18 23:22:29 +0900

    annotations

--- README.md
+++ README.md
@@ -4,10 +4,11 @@
 This repository contains resources developed by the JSPS KAKENHI project "A cross-linguistic study of pronoun substitutes and address terms" ([JP20H01255](https://kaken.nii.ac.jp/ja/grant/KAKENHI-PROJECT-20H01255/)).
 
 ## How to cite
-岡野賢二, 野元裕樹, スニサーウィッタヤーパンヤーノン, トゥザライン, 春日淳. 2022.[「アジア三言語における代名詞代用・呼びかけ語の共通項目調査」](https://www.anlp.jp/proceedings/annual_meeting/2022/pdf_dir/D1-2.pdf)『言語処理学会第28回年次大会発表論文集』, 69-73.
+- 岡野賢二, 野元裕樹, スニサーウィッタヤーパンヤーノン, トゥザライン, 春日淳. 2022.[「アジア三言語における代名詞代用・呼びかけ語の共通項目調査」](https://www.anlp.jp/proceedings/annual_meeting/2022/pdf_dir/D1-2.pdf)『言語処理学会第28回年次大会発表論文集』, 69-73.
+- 谷口龍子, 大久保弥, 野元裕樹, 南潤珍. 2022. [代名詞代用・呼びかけ表現の多言語データセット](https://www.ls-japan.org/modules/documents/LSJpapers/meeting/164/handouts/p/P-6_164.pdf).『日本言語学会第164回大会予稿集』, 307-313.
 
 ```bib
-@proceedings{OkanoEtAl22,
+@InProceedings{OkanoEtAl22,
     author = {Okano, Kenji and Nomoto, Hiroki and Wittayapanyanon, Sunisa and Thuzar Hlaing and Kasuga, Atsushi},
     year = {2022},
     title = {Ajia sangengo niokeru daimeishidaiyou, yobikakego no kyoutsuukoumoku chousa},
@@ -15,7 +16,17 @@ This repository contains resources developed by the JSPS KAKENHI project "A cros
     pages = {69-73},
     url = {https://www.anlp.jp/proceedings/annual_meeting/2022/pdf_dir/D1-2.pdf},
     note = {An investigation of pronoun substitutes and address terms in three Asian languages based on a common questionnaire}
-    }
+}
+
+@InProceedings{TaniguchiEtAl22,
+    author = {Taniguchi, Ryuko and Okubo, Wataru and Nomoto, Hiroki and Nam, Yunjin},
+    year = {2022},
+    title = {Daimeishidaiyou, yobikake hyougen no tagengo deetasetto},
+    booktitle = {The Proceedings of the 164th Meeting of the {L}inguistic {S}ociety of {J}apan},
+    pages = {307-313},
+    url = {https://www.ls-japan.org/modules/documents/LSJpapers/meeting/164/handouts/p/P-6_164.pdf},
+    note = {A multilingual dataset of pronoun substitutes and address terms}
+}
 ```
 
 ## Contents
---
+++ annotations/convert_format.py
@@ -0,0 +1,105 @@
+import json
+
+def doccano2UAM(jsonl,filename):
+    """
+    Convert doccano jsonl to UAM tab-delimited text
+    """
+    annotations = []
+#    check= open("corpus_validation.txt","w",encoding="utf-8")
+    with open(jsonl, "r", encoding="utf-8") as f:
+        json_list = list(f)
+        start = 0 #start index of the line in the entire file
+        for i in json_list:
+            item = json.loads(i)
+#            print(item["text"], file=check)
+            if item["label"]:
+                for l in item["label"]:
+                    ID,s,e,tag,sent = item["id"],l[0],l[1],l[2],item["text"]
+                    s = int(s) #index in each line
+                    e = int(e)
+                    text = sent[s:e]
+                    s = s + start #index in the entire file
+                    e = e + start
+                    annotations.append((ID,s,e,text,tag))
+            start += len(item["text"]) + 2 #Windows line break \r\n
+#    check.close()
+    with open(filename[:-4]+"-prosub.txt", "w", encoding="utf-8") as out:
+        print("Subcorp\t\Filename\tID\tStart\tEnd\tText\tComment\tRole\tParentID\tdoc_completeness\tprosub\t1st\t2nd\taddress", file=out)
+        for a in annotations:
+            ID,s,e,text,tag = a
+            if tag == "Speaker":
+                print("Texts",filename,ID,s,e,text,"\t\t\t\t1\t1\t0\t0", sep="\t",
+                        file=out)
+            elif tag == "Addressee":
+                print("Texts",filename,ID,s,e,text,"\t\t\t\t1\t0\t1\t0", sep="\t",
+                        file=out)
+            elif tag == "AddressTerm":
+                print("Texts",filename,ID,s,e,text,"\t\t\t\t1\t0\t0\t1", sep="\t",
+                        file=out)
+
+def ETA2doccano(summary):
+    """
+    Convert ETA summary table to doccano jsonl
+    """
+    filename = ""
+    text_length = 0 #need this b/c # of columns vary depending on texts
+    json = []
+    count = 0
+    with open(summary, "r", encoding="utf-8") as f:
+        for l in f:
+            items = l.strip("\n").split("\t")
+            if items[0] == "File Name":
+                filename = items[1]
+            elif items[0] == "Text":
+                text_length = len(items) - 8
+            elif items[0] and items[0] not in ["Feature", "Editor's Name"]:
+                text = "\t".join(items[:text_length])
+#                print(text)
+                labels = ""
+                if items[-7]:
+                    words = items[-7].split(";")
+#                    print(words)
+                    for w in words:
+                        s = text.index(w)
+                        e = s + len(w)
+                        if labels:
+                            labels += ",[{},{},{}]".format(str(s),str(e),'"1st"')
+                        else:
+                            labels += "[{},{},{}]".format(str(s),str(e),'"1st"')
+                if items[-5]:
+                    words = items[-5].split(";")
+#                    print(words)
+                    for w in words:
+                        s = text.index(w)
+                        e = s + len(w)
+                        if labels:
+                            labels += ",[{},{},{}]".format(str(s),str(e),'"2nd"')
+                        else:
+                            labels += "[{},{},{}]".format(str(s),str(e),'"2nd"')
+                if items[-3]:
+                    words = items[-3].split(";")
+#                    print(words)
+                    for w in words:
+                        s = text.index(w)
+                        e = s + len(w)
+                        if labels:
+                            labels += ",[{},{},{}]".format(str(s),str(e),'"address"')
+                        else:
+                            labels += "[{},{},{}]".format(str(s),str(e),'"address"')
+                text = text.replace("\t","\\t")
+                string = '{"id":'+str(count)+',"text":"'+text+'","label":['+labels+']}'
+                json.append(string)
+                count += 1
+    with open(filename[:-4]+"-prosub.jsonl", "w", encoding="utf-8") as out:
+        for j in json:
+            print(j, file=out)
+
+if __name__ == "__main__":
+    #jsonl to UAM
+    f = "./tha/data_tha-prosub.jsonl"
+    filename = "data_tha.txt"
+    doccano2UAM(f, filename)
+
+    #ETA to jsonl
+    f = "./jpn/data_jpn-summary.txt"
+    ETA2doccano(f)

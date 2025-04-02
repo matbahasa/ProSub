a02660447cbb578b8e7aa5354e1651a5baa95dc9
matbahasa <nomoto@tufs.ac.jp>
2025-04-03 00:29:02 +0900

    fixed the duplication problem

--- annotations/convert_format.py
+++ annotations/convert_format.py
@@ -42,7 +42,7 @@ def ETA2doccano(summary):
     Convert ETA summary table to doccano jsonl
     """
     filename = ""
-    text_length = 0 #need this b/c # of columns vary depending on texts
+    text_length = 0 #need this b/c # of columns varies depending on texts
     json = []
     count = 0
     with open(summary, "r", encoding="utf-8") as f:
@@ -51,7 +51,7 @@ def ETA2doccano(summary):
             if items[0] == "File Name":
                 filename = items[1]
             elif items[0] == "Text":
-                text_length = len(items) - 8
+                text_length = len(items) - 8 #1st, 2nd, 3rd, null * 2 = 8
             elif items[0] and items[0] not in ["Feature", "Editor's Name"]:
                 text = "\t".join(items[:text_length])
 #                print(text)
@@ -59,40 +59,47 @@ def ETA2doccano(summary):
                 if items[-7]:
                     words = items[-7].split(";")
 #                    print(words)
+                    current_position = 0 #where in the text to start searching from
                     for w in words:
                         if "【" in w: #exclude comments
                             w = w[:w.index("【")]
-                        s = text.index(w)
+                        s = text[current_position:].index(w) + current_position
                         e = s + len(w)
                         if labels:
                             labels += ",[{},{},{}]".format(str(s),str(e),'"1st"')
                         else:
                             labels += "[{},{},{}]".format(str(s),str(e),'"1st"')
+                        current_position = e
                 if items[-5]:
                     words = items[-5].split(";")
 #                    print(words)
+                    current_position = 0 #where in the text to start searching from
                     for w in words:
                         if "【" in w: #exclude comments
                             w = w[:w.index("【")]
-                        s = text.index(w)
+                        s = text[current_position:].index(w) + current_position
                         e = s + len(w)
                         if labels:
                             labels += ",[{},{},{}]".format(str(s),str(e),'"2nd"')
                         else:
                             labels += "[{},{},{}]".format(str(s),str(e),'"2nd"')
+                        current_position = e
                 if items[-3]:
                     words = items[-3].split(";")
 #                    print(words)
+                    current_position = 0 #where in the text to start searching from
                     for w in words:
                         if "【" in w: #exclude comments
                             w = w[:w.index("【")]
-                        s = text.index(w)
+                        s = text[current_position:].index(w) + current_position
                         e = s + len(w)
                         if labels:
                             labels += ",[{},{},{}]".format(str(s),str(e),'"address"')
                         else:
                             labels += "[{},{},{}]".format(str(s),str(e),'"address"')
+                        current_position = e
                 text = text.replace("\t","\\t")
+                text = text.replace('\"','\\"')
                 string = '{"id":'+str(count)+',"text":"'+text+'","label":['+labels+']}'
                 json.append(string)
                 count += 1
@@ -108,4 +115,5 @@ if __name__ == "__main__":
 
     ETA to jsonl
     f = "./jpn/data_jpn-summary.txt"
+    f = "data_jav-summary.txt"
     ETA2doccano(f)
